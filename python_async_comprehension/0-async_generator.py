070cef3c75fe1c2325beb29dbf40fb99644ef2e5
rukundo0023 <r.nshimiyim@alustudent.com>
2024-09-07 19:57:05 +0200

    Python - Async

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python3
 """ 0. Async Generator
 """
@@ -14,4 +13,3 @@ async def async_generator() -> Generator[float, None, None]:
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.random() * 10
-
