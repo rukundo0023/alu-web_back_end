835099e99ebdd0259160943a5dbd0efaec7fecec
rukundo0023 <r.nshimiyim@alustudent.com>
2024-09-07 18:59:04 +0200

    0-add.py

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -2,9 +2,8 @@
 """ 0. Simple type annotations - addition
 """
 
-def add(a: float, b: float) -> float:
-    """ Returns the sum of two float values.
-    """
-    return a + b
 
+def add(a: float, b: float) -> float:
+    """Returns the sum of two float values."""
+    return a + b
 
